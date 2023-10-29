e5c6954b7173673eb03f9dca36d785eb9c4bca97
Akash Holambe <akashholambe3@gmail.com>
2023-10-29 21:51:08 +0530

    concepts

---
+++ String.py
@@ -0,0 +1,23 @@
+'''
+In python we can slicie the Strings.
+
+Here in a string we perforemed slicing 
+'''
+a = "Akash Radhakishan Holambe  "
+print(a[2:10])
+print(a[-10:-2])
+
+'''
+Now we will see modifing the strings
+The upper() method returns the string in upper case.
+
+'''
+print(a.upper())
+
+"'printing lower case '"
+print(a.lower())
+
+#The strip() method removes any whitespace from the beginning or the end.
+
+print(a.strip()) # return "Akash Radhakishan Holambe"
+
---
+++ loops.py
@@ -0,0 +1,12 @@
+# for loop in python...
+'''
+for: The keyword that indicates the start of the loop.
+in: The keyword used to specify the sequence you want to iterate over.
+'''
+
+numbers = [1, 2, 3, 4, 5]  # List of the numbers
+
+for number in numbers:
+    print(number)
+
+
