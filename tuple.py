c779cf5496720dd507fd9dc679940338696ac10e
Akash Holambe <akashholambe1999@gmail.com>
2024-06-02 23:15:33 +0530

    Information about tuples

---
+++ tuple.py
@@ -0,0 +1,18 @@
+# Tuples are immutable data types eaning once they are created,
+# their elements cannot be changed. Here are some key characteristics and operations of tuples in Python:
+
+# Creating a tuple
+my_tuple = (1, 2, 3)
+print(my_tuple)  # Output: (1, 2, 3)
+
+# Creating a tuple without parentheses (comma-separated values)
+another_tuple = 4, 5, 6
+print(another_tuple)  # Output: (4, 5, 6)
+
+# Creating a tuple with a single element (comma is needed)
+single_element_tuple = (7,)
+print(single_element_tuple)  # Output: (7,)
+
+# Empty tuple
+empty_tuple = ()
+print(empty_tuple)  # Output: ()
